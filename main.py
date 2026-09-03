16c059c12cb89c4b413611cb227a167973e77970
Amin Ehsan <Mr.AminEhsan@gmail.com>
2026-09-03 21:28:16 +0330

    Add stream output

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from agents import Runner
 from asyncio import to_thread, run
+from agents import RawResponsesStreamEvent, Runner
+from openai.types.responses import ResponseTextDeltaEvent
 from agent import create_agent
 
 
 async def main() -> None:
     prompt = (await to_thread(input, "Task: ")).strip()
+    print()
     if not prompt:
         raise SystemExit("Task cannot be empty.")
     agent = create_agent()
-    result = await Runner.run(starting_agent=agent, input=prompt)
+    result = Runner.run_streamed(starting_agent=agent, input=prompt)
+    async for event in result.stream_events():
+        if (
+                isinstance(event, RawResponsesStreamEvent)
+                and isinstance(event.data, ResponseTextDeltaEvent)
+        ):
+            print(event.data.delta, end="", flush=True)
     usage = result.context_wrapper.usage
     print(
+        "\n\n"
         f"Token usage:\n"
-        f"input={usage.input_tokens}\n"
-        f"output={usage.output_tokens}\n"
-        f"total={usage.total_tokens}\n"
+        f"\tinput={usage.input_tokens}\n"
+        f"\toutput={usage.output_tokens}\n"
+        f"\ttotal={usage.total_tokens}"
     )
-    print(result.final_output)
 
 
 if __name__ == "__main__":
